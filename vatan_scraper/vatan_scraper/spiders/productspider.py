abb63e82f3fe2e350603cc8bf8c2a3140c3c5c5a
HayatiYrtgl <hayatiyurtoglu71@gmail.com>
2024-04-10 09:37:08 +0300

    first commit

--- vatan_scraper/vatan_scraper/spiders/productspider.py
+++ vatan_scraper/vatan_scraper/spiders/productspider.py
@@ -1,6 +1,7 @@
 import scrapy
 import json
 
+
 class ProductspiderSpider(scrapy.Spider):
     # name
     name = "productspider"
@@ -8,8 +9,13 @@ class ProductspiderSpider(scrapy.Spider):
     # domain
     allowed_domains = ["www.vatanbilgisayar.com"]
 
-    # url
-    start_urls = ["https://"]
+    start_urls = ["https://www.vatanbilgisayar.com/notebook/?page=1"]
 
     def parse(self, response):
-        pass
+        # url
+        with open("/home/charon5/web_scraping/web_scraping/vatan_scraper/vatan_scraper/products_urls.json", "r", encoding="utf-8") as file:
+            for url in json.loads(file.read()):
+                yield scrapy.Request(url=url["urls"], callback=self.parse_product)
+
+    def parse_product(self, response):
+        yield {"m": response.css("ul.pdetail-property-list li::text").get()}
